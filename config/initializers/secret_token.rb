8221848cc4f15b65ae608293b3a4333bd3856313
Mauricio A <malarcon@medivo.com>
2014-05-07 17:03:07 -0500

    initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f02a0cb3694084cba092ffc12ef9bddfc8a6308c21378d8df6bddf3899c3a3a7a582de5f878f6e113532b80951ee59af0a864b1c6db89c53b71a129515ad979f'
